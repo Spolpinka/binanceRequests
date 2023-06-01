190449585585a418fa031eb37f504166eb98689b
Spolpinka <a421243266@gmail.com>
2023-06-01 10:29:14 +0300

    парсит рубль и песо реализован запрос каждые 30 минут нет символов для обращения к фиатным валютам, кроме рубля реализована сериализация и десериализация LocalDateTime но график так и не реализован база в SQL

--- src/main/java/com/muh/binancerequests/controller/RequestController.java
+++ src/main/java/com/muh/binancerequests/controller/RequestController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 import java.util.TreeMap;
 
 @RestController
+//@RequestMapping("/api")
 public class RequestController {
 
     private final RequestService requestService;
