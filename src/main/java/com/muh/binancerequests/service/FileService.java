a67ebf69b9f4e401496dda9a2629d06eca9abd64
Spolpinka <a421243266@gmail.com>
2023-06-06 09:30:39 +0300

    парсит рубль и песо реализован запрос каждые 30 минут нет символов для обращения к фиатным валютам, кроме рубля реализована сериализация и десериализация LocalDateTime но график так и не реализован база в SQL

--- src/main/java/com/muh/binancerequests/service/FileService.java
+++ src/main/java/com/muh/binancerequests/service/FileService.java
@@ -15,31 +15,6 @@ public class FileService {
     @Value("${name.of.data.file}")
     private String dataFileName;
 
-
-    /*public String readFromFile() {
-        try {
-            return Files.readString(Path.of(dataFilePath, dataFileName));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return e.toString();
-        }
-    }*/
-
-    /*public boolean saveToFile(String json) {
-        try {
-            if (cleanDataFile()) {
-                Files.writeString(Path.of(dataFilePath, dataFileName), json);
-            } else {
-                System.out.println("Проблема с очищением файла записи");
-            }
-
-            return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }*/
-
     public boolean cleanDataFile() {
         try {
             Path path = Path.of(dataFilePath, dataFileName);
